e392aceff88be3d6f0144c81534f9baa12389752
Michel Novus <michelnovus@gmail.com>
2024-01-30 18:16:21 -0300

    Definida ventana raíz.

--- localdoc_tk/application.py
+++ localdoc_tk/application.py
@@ -1,5 +1,16 @@
 # [MIT License] Copyright (c) 2024 Michel Novus
 
+import customtkinter as ctk
+
+
+class Application(ctk.CTk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
 
 def main():
-    pass
+    ctk.set_appearance_mode("dark")
+    ctk.set_default_color_theme("green")
+
+    app = Application()
+    app.mainloop()
