d7b754e24cfaf11a8c64145d98a56062ee6a0bd1
Michel Novus <michelnovus@gmail.com>
2024-01-30 18:53:16 -0300

    Añadido marco de listado de paquetes.

--- localdoc_tk/application.py
+++ localdoc_tk/application.py
@@ -23,11 +23,24 @@ class StatusBar(ctk.CTkFrame):
         version_label.grid(row=0, column=2, padx=15, sticky="e")
 
 
+class PackageStack(ctk.CTkFrame):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
 class Application(ctk.CTk):
     def __init__(self, app_version: str, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.title("Localdoc")
         self.bind("<Control-q>", lambda event: self.destroy())
+
+        self.package_stack = PackageStack(
+            master=self,
+            corner_radius=15,
+            fg_color="#191919",
+            border_width=2,
+            border_color="#a0a0a0",
+        )
 
         self.status_bar = StatusBar(
             master=self,
@@ -39,6 +52,9 @@ class Application(ctk.CTk):
 
         self.grid_columnconfigure(1, weight=1)
         self.grid_rowconfigure(1, weight=1)
+        self.package_stack.grid(
+            row=1, column=1, padx=12, pady=12, sticky="nsew"
+        )
         self.status_bar.grid(row=2, column=1, padx=0, pady=0, sticky="ew")
 
 
