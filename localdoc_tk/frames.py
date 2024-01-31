6dee43ff2b6103ca5cafa5a0fcb1fbeb86723854
Michel Novus <michelnovus@gmail.com>
2024-01-31 17:36:31 -0300

    Ajustes en la clase Package.

--- localdoc_tk/frames.py
+++ localdoc_tk/frames.py
@@ -72,6 +72,19 @@ class Package(ctk.CTkFrame):
         super().__init__(*args, **kwargs)
         self.configure(corner_radius=20)
 
+        self.colors = {
+            "serve_button_not_served": {
+                "fg_color": "transparent",
+                "hover_color": "#00551c",
+                "border_color": "#cccccc",
+            },
+            "serve_button_served": {
+                "fg_color": "#6e0000",
+                "hover_color": "#550000",
+                "border_color": "#cccccc",
+            },
+        }
+
         self.images = images
         self.position = position
 
@@ -91,19 +104,18 @@ class Package(ctk.CTkFrame):
             border_width=0,
         )
         self._state = False
-        self.on_off_button = ctk.CTkButton(
+        self.serve_button = ctk.CTkButton(
             master=self,
             text="",
             corner_radius=5,
             border_width=1,
-            border_color="#cccccc",
+            border_color=self.colors["serve_button_not_served"]["border_color"],
             image=self.images.get("check-filled.svg", (26, 26)),
             compound="top",
-            fg_color="transparent",
-            hover_color="#006622",
+            fg_color=self.colors["serve_button_not_served"]["fg_color"],
+            hover_color=self.colors["serve_button_not_served"]["hover_color"],
             width=0,
             border_spacing=4,
-            command=self._change_check,
         )
 
         self.grid_columnconfigure(0, minsize=30)
@@ -111,22 +123,37 @@ class Package(ctk.CTkFrame):
         self.grid_columnconfigure(2, minsize=5)
         self.served_in.grid(row=0, column=3, padx=0, sticky="e")
         self.grid_columnconfigure(4, weight=1)
-        self.on_off_button.grid(row=0, column=5, padx=10, pady=5, sticky="nsew")
+        self.serve_button.grid(row=0, column=5, padx=10, pady=5, sticky="nsew")
         self.grid_columnconfigure(6, weight=1)
 
-    # TEMP
-    def _change_check(self, event=None):
-        if self._state:
-            self.on_off_button.configure(
-                image=self.images.get("check-filled.svg", (25, 25))
+    def change_state_serve_button(self, state: str):
+        """Change color and symbol state of serve_button. The parameter state
+        should be string: {"SERVED" | "NOT_SERVED"}."""
+        if state == "NOT_SERVED":
+            self.serve_button.configure(
+                image=self.images.get("check-filled.svg", (26, 26))
             )
-            self.on_off_button.configure(fg_color="transparent")
-            self.on_off_button.configure(hover_color="#006622")
+            self.serve_button.configure(
+                fg_color=self.colors["serve_button_not_served"]["fg_color"]
+            )
+            self.serve_button.configure(
+                hover_color=self.colors["serve_button_not_served"][
+                    "hover_color"
+                ]
+            )
             self._state = False
-        else:
-            self.on_off_button.configure(
-                image=self.images.get("close-filled.svg", (25, 25))
+        elif state == "SERVED":
+            self.serve_button.configure(
+                image=self.images.get("close-filled.svg", (26, 26))
             )
-            self.on_off_button.configure(fg_color="#990000")
-            self.on_off_button.configure(hover_color="#660000")
+            self.serve_button.configure(
+                fg_color=self.colors["serve_button_served"]["fg_color"]
+            )
+            self.serve_button.configure(
+                hover_color=self.colors["serve_button_served"]["hover_color"]
+            )
             self._state = True
+        else:
+            raise ValueError(
+                'state parameter should be: {"SERVED" | "NOT_SERVED"}'
+            )
